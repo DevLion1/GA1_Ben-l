e6ec852ac73d28ff8da813362a0ab22b5b654d72
Benel Alcine <130769411+DevLion1@users.noreply.github.com>
2023-10-21 06:17:35 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace GA1_Benél
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+
+        {
+            InitializeComponent();//Don't delete this
+            MessageBox.Show("Benél, I want to be a Developer when I graduate");
+        }
+    }
+}
